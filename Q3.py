15da714caaba264b11c70f76edd7476b75511290
Vice Plr <th91rb@gmail.com>
2026-03-24 14:58:31 +0700

    Add additional test cases for knight_move function
    Updated test cases for knight moves in a 4x4 matrix.

--- Q3.py
+++ Q3.py
@@ -5,7 +5,7 @@ def knight_move(x,y):
         if 0 <= x + move[0] < m and 0 <= y + move[1] < n:
             a.append((x + move[0],y + move[1]))
     return a
-test=[[1,1]]
+test=[[1,1],[0,1],[2,1],[3,2]]
 m,n=4,4
 for i in test:
     print(f'Valid move of knight in matrix A {m}x{n}: {knight_move(i[0],i[1])}')
